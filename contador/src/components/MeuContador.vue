040252a208358bff0abf712ed3178794f2c183c2
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-03-16 10:50:17 -0400

    cria componente MeuContador.js

---
+++ contador/src/components/MeuContador.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="meu-contador">
+        <p><strong>Contador: </strong>{{contadorAtual}}</p>
+        <button v-on:click = subtrairUm()>-</button>     
+        <button v-on:click = somarUm()>+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'MeuContador', 
+    data() {
+        return{
+            contador:0
+        }
+    },
+    methods: {
+        subtrairUm(){
+            if(contador>0){
+                this.contador--;
+            }
+        },
+        somarUm(){
+            this.contador++;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
