7b346a9a1650d9d51d486f003a4d79613e209a58
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-03-17 09:23:06 -0400

    adiciona o componente SitesFavoritos.vue ao App.vue

--- api-sites-favoritos/src/App.vue
+++ api-sites-favoritos/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SitesFavoritos/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SitesFavoritos from './components/SitesFavoritos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SitesFavoritos
   }
 }
 </script>
